79c5f76f03b777dc356cb4b5d8876b885cdf7cf0
DevWBG <DevWBG@gmail.com>
2013-12-04 20:12:38 +0900

    Fix Copyright

--- WindowsFormsApplication1/Properties/AssemblyInfo.cs
+++ WindowsFormsApplication1/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ClipboardImageViewer")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © WBG 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
